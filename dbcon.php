92a26b692f2a9ebce3f45d54e87e0d983a10205c
happyshravan <shravan704@gmail.com>
2016-02-29 14:12:50 +0530

    yoyoyo

---
+++ dbcon.php
@@ -0,0 +1,6 @@
+<?php
+//
+$page='index.php';
+mysql_connect('localhost','root','tiger')or die(mysql_error());
+mysql_select_db('test_db')or die(mysql_error());
+?>
---
+++ librarian/dbcon.php
@@ -0,0 +1,3 @@
+<?php
+mysql_select_db('test_db',mysql_connect('localhost','root','tiger'))or die(mysql_error());
+?>
